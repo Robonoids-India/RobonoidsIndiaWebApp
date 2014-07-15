b1fed2c6837f18e82a262a05b40d939a70b9dabe
abmedury <medury_ajay@hotmail.com>
2014-07-15 14:20:46 +0530

    started building controller

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,1 +1,20 @@
-var RIWebApp = angular.module('RIWebApp', []);
+var RIWebApp = angular.module('RIWebApp', ['ngRoute', 'ngResource']);
+
+// manage routing and controllers
+/*dashApp.config(['$routeProvider', '$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider) {
+    // set up routes
+    $routeProvider
+        .when('/', {
+            controller: 'listCtrl',
+            templateUrl: 'partials/listview.html'
+        })
+        .when('/about', {
+            controller: 'docCtrl',
+            templateUrl: 'partials/docview.html'
+        })
+        .otherwise({ redirectTo: '/' });
+
+    // send requests with appropriate CORS headers for the whole app
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);*/
